2f38411871a7d1a14b83c308234ae7c0dbe08cd0
shaimawaz <161009@ppu.edu.ps>
2020-02-12 21:41:27 +0200

    changed the way of work and a connection has been made

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res) {
-   res.sendfile('./index.html');
-});
-
-users = [];
-io.on('connection', function(socket) {
-   console.log('A user connected');
-//    socket.on('setUsername', function(data) {
-//       if(users.indexOf(data) > -1) {
-//          users.push(data);
-//          socket.emit('userSet', {username: data});
-//       } else {
-//          socket.emit('userExists', data + ' username is taken! Try some other username.');
-//       }
-//    })
-});
-
-http.listen(3000, function() {
+var express = require('express');
+var socket = require('socket.io');
+var app = express();
+var server = app.listen(4000, function(){
    console.log('listening on localhost:3000');
 });
+app.use(express.static('public'));
+var io = socket(server);
+io.on('connection', (socket) => {
+    console.log('a user has connected');
+    socket.on('addTask', function(data){
+      // console.log(data);
+      io.sockets.emit('addTask', data);
+  });
+});
+
